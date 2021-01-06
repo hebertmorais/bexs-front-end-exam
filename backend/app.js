d12acb7490909ba87bc384c291e38190d0c946a5
Hebert Morais <hebertlucashllm@gmail.com>
2021-01-05 22:05:02 -0300

    Added cors to backend

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+var cors = require("cors");
+const app = express();
+const port = 3000;
 
-app.post('/pagar', (req, res) => {
-  res.send({'status': 'success'})
-})
+app.use(cors());
+
+app.post("/pagar", (req, res) => {
+  res.send({ status: "success" });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
